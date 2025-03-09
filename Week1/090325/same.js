3db805065354d83b7bc65fefbd0a7741aec43409
Yao  Lu <yao.lu@beamex.com>
2025-03-09 20:56:26 +0200

    same.js solved O(n^1)

--- Week1/090325/same.js
+++ Week1/090325/same.js
@@ -10,43 +10,40 @@ same([1,2,1],[4,4,1]) //false
 */
 
 function same(array1, array2) {
-//   for (item of array1) {
-//     const index = array2.indexOf(item ** 2);
-//     if (index != -1) {
-//       array2.splice(index, 1);
-//     }
-//   }
-
-//   if (array2.length > 0) {
-//     return false;
-//   } else {
-//     return true;
-//   }
-
-for(let i=0;i<array1.length;i++){
-    const foundIndex = array2.indexOf(array1[i]**2)
-    if(foundIndex<0)return false 
-    else{
-        array2.splice(foundIndex,1)
-
+  if (array1.length != array2.length) return false;
+  //Try to use frequency counter
+  frequencyCounter = {};
+  squaredArray = array1.forEach((item) => {
+    squaredItemString = String(item ** 2);
+    if (!(squaredItemString in frequencyCounter)) {
+      frequencyCounter[squaredItemString] = 1;
+    } else {
+      frequencyCounter[squaredItemString] += 1;
     }
-}
-if (array2.length>0){
-    return false 
-} 
-else{
-    return true 
-}
-}
-console.log(same([1, 2, 3], [1, 4, 9]));     // Expected: true
-console.log(same([1, 2, 3], [9, 4, 1]));     // Expected: true (order doesn't matter)
-console.log(same([1, 2, 3], [1, 9]));        // Expected: false (missing one square)
-console.log(same([1, 2, 2], [1, 4, 4]));     // Expected: true (frequency matches)
-console.log(same([1, 2, 2], [1, 4, 9]));     // Expected: false (extra wrong value)
-console.log(same([], []));                   // Expected: true (both arrays empty)
-console.log(same([], [1]));                  // Expected: false (extra value in second array)
-console.log(same([3, 3, 3], [9, 9, 9]));     // Expected: true
-console.log(same([3, 3, 3], [9, 9]));        // Expected: false (missing one 9)
-console.log(same([2, 3], [4, 9, 9]));        // Expected: false (extra 9)
+  });
 
+  for (const item of array2) {
+    if (String(item) in frequencyCounter) {
+      frequencyCounter[String(item)] -= 1;
+    }
+  }
 
+  let isSame = true;
+  for (const prop in frequencyCounter) {
+    if (frequencyCounter[prop] != 0) {
+      isSame = false;
+      break;
+    }
+  }
+  return isSame;
+}
+console.log(same([1, 2, 3], [1, 4, 9])); // Expected: true
+console.log(same([1, 2, 3], [9, 4, 1])); // Expected: true (order doesn't matter)
+console.log(same([1, 2, 3], [1, 9])); // Expected: false (missing one square)
+console.log(same([1, 2, 2], [1, 4, 4])); // Expected: true (frequency matches)
+console.log(same([1, 2, 2], [1, 4, 9])); // Expected: false (extra wrong value)
+console.log(same([], [])); // Expected: true (both arrays empty)
+console.log(same([], [1])); // Expected: false (extra value in second array)
+console.log(same([3, 3, 3], [9, 9, 9])); // Expected: true
+console.log(same([3, 3, 3], [9, 9])); // Expected: false (missing one 9)
+console.log(same([2, 3], [4, 9, 9])); // Expected: false (extra 9)
