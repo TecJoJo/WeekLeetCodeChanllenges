c3a3462f4f4a7ab474e6a3288be39d9b612d69c9
Yao Lu Home Win11 <luyao2493751@gmail.com>
2025-04-10 21:35:52 +0300

    factorial number solved using recursion

---
+++ Week3_0304_1004/1004/factorial.js
@@ -0,0 +1,22 @@
+//factorial(1) // 1
+// factorial(2) // 2
+// factorial(4) // 24
+// factorial(7) // 5040
+
+function factorial(n){
+   //base case
+   if(n ===0) return 1
+   //not sure yet
+    return n * factorial(n-1)
+
+   
+}
+
+
+console.log(factorial(1)) // 1
+console.log(factorial(2)) // 2
+console.log(factorial(4)) // 24
+console.log(factorial(7)) // 5040
+
+
+//solved completely on my own
