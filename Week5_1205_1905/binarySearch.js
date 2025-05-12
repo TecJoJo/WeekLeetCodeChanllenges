1b1dc4da43d68571799a64e3500ce73bc0bf6a87
Yao Laptop <luyao2493751@gmail.com>
2025-05-12 22:51:31 +0300

    binary seach

---
+++ Week5_1205_1905/binarySearch.js
@@ -0,0 +1,69 @@
+// Binary Search Exercise
+// Write a function called binarySearch which accepts a sorted array and a value and returns the index at which the value exists. Otherwise, return -1.
+
+// This algorithm should be more efficient than linearSearch - you can read how to implement it here - https://www.khanacademy.org/computing/computer-science/algorithms/binary-search/a/binary-search and here - https://www.topcoder.com/community/data-science/data-science-tutorials/binary-search/
+
+// Examples:
+
+// binarySearch([1,2,3,4,5],2) // 1
+// binarySearch([1,2,3,4,5],3) // 2
+// binarySearch([1,2,3,4,5],5) // 4
+// binarySearch([1,2,3,4,5],6) // -1
+// binarySearch([
+//   5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+//   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+// ], 10) // 2
+// binarySearch([
+//   5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+//   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+// ], 95) // 16
+// binarySearch([
+//   5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+//   40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+// ], 100) // -1
+
+function binarySearch(sortedNums, num) {
+  let start = 0;
+  let end = sortedNums.length-1;
+  let currentIndex = Math.floor(end/2)
+  while (true) {
+    
+    if (sortedNums[currentIndex] === num) return currentIndex;
+
+    if (sortedNums[start] === num) return start;
+
+    if (sortedNums[end] === num) return end;
+
+    if(end - start ===1) return -1
+
+    if (num > sortedNums[currentIndex]) {
+      start = currentIndex;
+    } else {
+      end = currentIndex;
+    }
+    currentIndex = Math.floor((end - start) / 2) + start;
+    
+  }
+}
+
+
+binarySearch([1,2,3,4,5],2) // 1
+binarySearch([1,2,3,4,5],3) // 2
+binarySearch([1,2,3,4,5],5) // 4
+binarySearch([1,2,3,4,5],6) // -1
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 10) // 2
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 95) // 16
+binarySearch([
+  5, 6, 10, 13, 14, 18, 30, 34, 35, 37,
+  40, 44, 64, 79, 84, 86, 95, 96, 98, 99
+], 100) // -1
+
+
+
+//solved completely by myself
